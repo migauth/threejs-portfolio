474bca27a989335205ac04bbbe1f93fc15335af4
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-15 14:40:52 -0700

    add navbar template, canvas and basic test, postprocessing and rapier

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,32 @@
 import "./App.css";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Navbar from "./components/Navbar";
+
+function Scene() {
+  <Canvas camera={{ position: [-1, 4, 2.5] }}>
+    <directionalLight position={[1, 1, 1]} />
+    <mesh>
+      <icosahedronGeometry args={[1, 1]} />
+    </mesh>
+  </Canvas>;
+}
 
 function App() {
   return (
     <main className="bg-white h-screen w-screen">
-     <h1>hi</h1>
+      <h1>hi</h1>
+      <Navbar />
+      <div className="border border-black rounded-3xl m-10 bg-black">
+        <Canvas camera={{ position: [-1, 4, 2.5] }}>
+          <directionalLight position={[1, 1, 1]} />
+          <mesh >
+            <icosahedronGeometry args={[1, 1]} />
+            <meshBasicMaterial color={'lime'} wireframe/>
+          </mesh>
+          <OrbitControls />
+        </Canvas>
+      </div>
     </main>
   );
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h1>navbar</h1>
+      <div>
+        <button className="text-gray-400 p-5 border border-black">About</button>
+        <button className="text-gray-400 p-5 border border-black">Resume</button>
+        <button className="text-gray-400 p-5 border border-black">Projects</button>
+        <button className="text-gray-400 p-5 border border-black">Contact</button>
+      </div>
+    </nav>
+  )
+};
