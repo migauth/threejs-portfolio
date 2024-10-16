d582c732cfce1186e16315a5fcc5bd44047a5262
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-16 09:52:40 -0700

    add navbar test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,6 @@
 export default function Navbar() {
   return (
     <nav>
-      <h1>navbar</h1>
       <div>
         <button className="text-gray-400 p-5 border border-black">About</button>
         <button className="text-gray-400 p-5 border border-black">Resume</button>
