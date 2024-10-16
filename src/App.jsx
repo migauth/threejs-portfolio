6800b07e7cec225eca7c641f9fe1241698ac7e61
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-16 09:51:54 -0700

    add gltf loader and useloader/add test name model to scene function w primative

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,37 @@
 import "./App.css";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
 import Navbar from "./components/Navbar";
 
 function Scene() {
-  <Canvas camera={{ position: [-1, 4, 2.5] }}>
-    <directionalLight position={[1, 1, 1]} />
-    <mesh>
-      <icosahedronGeometry args={[1, 1]} />
-    </mesh>
-  </Canvas>;
+  const gltf = useLoader(GLTFLoader, "/models/3dname.glb");
+
+  return (
+    <Canvas camera={{ position: [-1, 4, 2.5] }}>
+      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI * 1} />
+      <mesh>
+        {/* <icosahedronGeometry args={[1, 1]} /> */}
+        {/* <meshBasicMaterial color={"lime"} wireframe /> */}
+        <primitive object={gltf.scene} position={[0, 1, 0]} />
+      </mesh>
+      <OrbitControls />
+    </Canvas>
+  );
 }
 
 function App() {
   return (
     <main className="bg-white h-screen w-screen">
-      <h1>hi</h1>
-      <Navbar />
-      <div className="border border-black rounded-3xl m-10 bg-black">
-        <Canvas camera={{ position: [-1, 4, 2.5] }}>
-          <directionalLight position={[1, 1, 1]} />
-          <mesh >
-            <icosahedronGeometry args={[1, 1]} />
-            <meshBasicMaterial color={'lime'} wireframe/>
-          </mesh>
-          <OrbitControls />
-        </Canvas>
+      <header className="flex justify-between m-16">
+        <div id="logo">
+          <h1>Michael Gauthier</h1>
+        </div>
+        <Navbar />
+      </header>
+
+      <div className="border border-black rounded-3xl m-16 ">
+        <Scene />
       </div>
     </main>
   );
