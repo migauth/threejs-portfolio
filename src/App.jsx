b53d316362687befe84ffc01ce5e7ae7d99326cb
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-17 11:45:39 -0700

    add name component, new font glb and name roation

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,31 @@
 import "./App.css";
-import { Canvas, useLoader } from "@react-three/fiber";
+import { Canvas, useLoader, useFrame } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { useRef } from "react";
 import Navbar from "./components/Navbar";
 
+// I have to create another component for my name to spin so the canvas can render first.
+
+function Name() {
+  const gltf = useLoader(GLTFLoader, "/models/3dnamePS.glb");
+  const ref = useRef()
+
+  useFrame((_, delta) => {
+    ref.current.rotation.y += 1 * delta
+  })
+  return (
+    <primitive object={gltf.scene} position={[0, 1, 0]} ref={ref} />
+
+  )
+}
+
 function Scene() {
-  const gltf = useLoader(GLTFLoader, "/models/3dname.glb");
 
   return (
-    <Canvas camera={{ position: [-1, 4, 2.5] }}>
-      <directionalLight position={[3.3, 1.0, 4.4]} intensity={Math.PI * 1} />
-      <mesh>
-        {/* <icosahedronGeometry args={[1, 1]} /> */}
-        {/* <meshBasicMaterial color={"lime"} wireframe /> */}
-        <primitive object={gltf.scene} position={[0, 1, 0]} />
-      </mesh>
+    <Canvas camera={{ position: [-0.7, 0.8, 2.5] }}>
+      <directionalLight position={[1, 1, 1]} intensity={Math.PI * 1} />
+      <Name />
       <OrbitControls />
     </Canvas>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,9 @@
 
-
 export default function Navbar() {
   return (
     <nav>
       <div>
-        <button className="text-gray-400 p-5 border border-black">About</button>
+        <button className="text-gray-400 p-5 border border-black" >About</button>
         <button className="text-gray-400 p-5 border border-black">Resume</button>
         <button className="text-gray-400 p-5 border border-black">Projects</button>
         <button className="text-gray-400 p-5 border border-black">Contact</button>
