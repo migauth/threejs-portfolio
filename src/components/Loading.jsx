43a7985b8c0efc18121bc1249dac5bf3c78f276f
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-22 10:09:44 -0700

    add loading gif

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,12 @@
+import "./Loading.css";
+
 export default function Loading() {
   return (
-    <div className="flex w-full h-full justify-center items-center">
-      <p className="text-4xl">
-        Loading
-      </p>
-    </div>
+    <>
+      <div className="flex flex-col w-full h-full justify-center items-center">
+        <p className="text-2xl font-mono">Loading</p>
+        <span className="loader"></span>
+      </div>
+    </>
   );
 }
