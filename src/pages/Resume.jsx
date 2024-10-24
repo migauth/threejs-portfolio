971ac5cc0d7ed725a9a695b0f2adc34e2f7c59e1
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-24 12:41:23 -0700

    rm resume page

--- src/pages/Resume.jsx
+++
@@ -1,9 +0,0 @@
-export default function Resume() {
-  return (
-    <div>
-      <a id="resume" href="./documents/Michael_Gauthier_Resume.pdf" download>
-        click here to download my resume
-      </a>
-    </div>
-  );
-}
