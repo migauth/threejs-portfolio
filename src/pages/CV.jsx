f1d8c71c1af5ed1652f06c315d267fe86f0fdd79
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-24 12:41:12 -0700

    change name to CV

---
+++ src/pages/CV.jsx
@@ -0,0 +1,9 @@
+export default function Resume() {
+  return (
+    <div>
+      <a id="resume" href="./documents/Michael_Gauthier_Resume.pdf" download>
+        click here to download my resume
+      </a>
+    </div>
+  );
+}
