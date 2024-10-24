df31db627879e8e1eae14565c326eb6e37c087d1
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-24 12:40:04 -0700

    remove flex col for nav bar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,20 +3,15 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <>
-      <div>
-        <h1 id="logo" className="text-2xl">
+      <nav className="justify-around w-1/2 text-2xl sm:flex-col">
+        <h1 id="logo" className="px-4">
           <Link to="/">Michael Gauthier</Link>
         </h1>
-      </div>
-      <nav className="justify-around w-1/2 text-2xl">
         <button className="px-4">
           <Link to="/about">About</Link>
         </button>
         <button className="px-4">
-          <Link to="/resume">Resume</Link>
-        </button>
-        <button className="px-4">
-          <Link to="/projects">Projects</Link>
+          <Link to="/resume">CV</Link>
         </button>
         <button className="px-4">
           <Link to="/contact">Contact</Link>
