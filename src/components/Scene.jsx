56dbeb129896087810df9dfc22585ebae08d13fc
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-27 12:55:45 -0700

    add cool box thing and about description

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,25 +1,40 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef } from "react";
+import * as THREE from "three";
 
 function Sphere() {
-
   const ref = useRef();
 
   useFrame((_, delta) => {
     ref.current.rotation.y += 0.1 * delta;
   });
 
+  // Animate the sphere vertices
+  useFrame(({ clock }) => {
+    if (ref.current) {
+      const time = clock.getElapsedTime();
+      const positions = ref.current.geometry.attributes.position.array;
+
+      for (let i = 0; i < positions.length; i += 4) {
+        positions[i] += 0.00005 * Math.sin(time + i); // x-coordinate
+        positions[i + 1] += 0.000009 * Math.sin(time + i * 1.1); // y-coordinate
+        positions[i + 2] += 0.005 * Math.sin(time + i * 1.2); // z-coordinate
+      }
+
+      ref.current.geometry.attributes.position.needsUpdate = true;
+    }
+  });
+
   return (
     <points ref={ref}>
-      <sphereGeometry args={[1, 200, 200]} />
+      <boxGeometry args={[1.2, 1.2, 1.2, 200, 200, 200]} />
       <pointsMaterial color="#A855F7" size={0.005} sizeAttenuation transparent opacity={0.7}/>
     </points>
   );
 }
 
 export default function Scene() {
-
   return (
     <Canvas camera={{ position: [1.5, 1.5, 1.5] }}>
       <ambientLight intensity={0.5} />
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,14 +4,18 @@ import { faSmile } from "@fortawesome/free-solid-svg-icons";
 export default function About() {
   return (
     <section className="about w-full h-screen relative p-12 pt-48">
-      <FontAwesomeIcon className="text-8xl pb-4 text-purple-200 hover:text-white" icon={faSmile} />
+      <FontAwesomeIcon
+        className="text-8xl pb-4 text-purple-200 hover:text-white"
+        icon={faSmile}
+      />
+      <span>Who am I</span>
       <p className="bio text-4xl  sm:text-4xl">
-        Hello! I’m a full-stack web developer with a diverse background in
+        I’m a full-stack web developer with a diverse background in
         inclusive education and creative practices. My passions span across many
         projects, with a goal of working in 3D web or game development. I’m
         dedicated to crafting effective, adaptable, and test-driven code and
-        value both community engagement and clear communication. Plus, I’m a fan
-        of pixel art, pizza, and sushi.
+        value both community engagement and clear communication. When I'm not coding, I’m a gaming
+        climbing, cooking, or eating pizza.
       </p>
     </section>
   );
