f62febc98e207cfbc52cda0998ec1afd9923798b
Michael Gauthier <mikegauthier222@hotmail.com>
2024-10-29 12:28:16 -0700

    test bloom

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -2,45 +2,31 @@
 import { Canvas, useFrame } from "@react-three/fiber";
 import { useRef, useState, useEffect } from "react";
 
-function Sphere() {
+
+function Box() {
   const ref = useRef();
 
-  const [hovered, setHovered] = useState(false)
-
-  useEffect(() => {
-    if (ref.current) {
-      const positions = ref.current.geometry.attributes.position.array;
-
-      // Access a specific point by index, where each point has an x, y, z coordinate
-      const pointIndex = 0; // Replace this with the index of the point you want
-      const x = positions[pointIndex * 3];
-      const y = positions[pointIndex * 3 + 1];
-      const z = positions[pointIndex * 3 + 2];
-      console.log("Point coordinates:", { x, y, z });
-    }
-  }, [ref]);
-
-  // const [hovered, hover] = useState(false);
+  const [hovered, setHovered] = useState(false);
 
   useFrame((_, delta) => {
     ref.current.rotation.y += 0.1 * delta;
   });
 
-  //access the points of each position on its axis with a position varible?
-
   return (
     <points
       ref={ref}
-      // onPointerOver={(event) => (event.stopPropagation(), hover(true))}
+      onPointerMove={(e) => {
+        e.stopPropagation();
+        console.log("Hover index of component:", e.index);
+        setHovered(true);
+      }}
+      onPointerOut={() => setHovered(false)}
     >
       <boxGeometry args={[1, 1, 1, 20, 20, 20]} />
       <pointsMaterial
-        // color={hovered ? "#A855F7" : "orange"}
-        color="#A855F7"
-        size={0.01}
-        sizeAttenuation
-        transparent
-        opacity={0.7}
+        color={hovered ? "#A855F7" : "orange"}
+        // color="#A855F7"
+        size={0.02}
       />
     </points>
   );
@@ -50,7 +36,7 @@ export default function Scene() {
   return (
     <Canvas camera={{ position: [0, 0, 1.9] }}>
       <ambientLight intensity={0.5} />
-      <Sphere />
+      <Box />
     </Canvas>
   );
 }
