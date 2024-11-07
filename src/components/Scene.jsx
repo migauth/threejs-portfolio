870157b22e03fe3607a852e2d1e79dab16874c90
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-07 09:32:05 -0800

    rm orbital controls and fix cube

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable react/no-unknown-property */
 import { Canvas, useFrame } from "@react-three/fiber";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import * as THREE from "three";
-import { OrbitControls } from "@react-three/drei";
 
 // purple color "#A855F7"
 
@@ -10,19 +9,16 @@ const tempObject = new THREE.Object3D();
 
 function Spheres() {
   const meshRef = useRef();
-  const [hovered, setHovered] = useState(false);
-  const count = 100
+  const count = 1000;
 
-  useFrame((_, delta) => {
-    meshRef.current.rotation.y += 0.1 * delta;
-
+  useFrame(() => {
     for (let i = 0; i < count; i++) {
       // Calculate the position based on a cubic arrangement
       const x = (i % 10) * 1.2 - 6; // Adjust spacing and position
       const y = Math.floor((i % 100) / 10) * 1.2 - 6;
       const z = Math.floor(i / 100) * 1.2 - 6;
 
-      tempObject.position.set(x, y, Math.floor(i / 10) * 1.2 - 6); // Now this creates a depth effect
+      tempObject.position.set(x, y, z); // Now this creates a depth effect
       tempObject.updateMatrix();
       meshRef.current.setMatrixAt(i, tempObject.matrix);
     }
@@ -31,16 +27,7 @@ function Spheres() {
   });
 
   return (
-    <instancedMesh
-      ref={meshRef}
-      args={[null, null, count]}
-      onPointerMove={(e) => {
-        e.stopPropagation();
-        console.log("Hover index of component:", e.index);
-        setHovered(true);
-      }}
-      onPointerOut={() => setHovered(false)}
-    >
+    <instancedMesh ref={meshRef} args={[null, null, count]}>
       <sphereGeometry args={[0.1, 10, 10]} />
       <meshBasicMaterial color={"#A855F7"} />
     </instancedMesh>
@@ -49,10 +36,9 @@ function Spheres() {
 
 export default function Scene() {
   return (
-    <Canvas camera={{ position: [0, 0, 15] }}>
+    <Canvas camera={{ position: [0, 0, 20] }}>
       <ambientLight intensity={0.1} />
       <Spheres />
-      <OrbitControls />
     </Canvas>
   );
 }
