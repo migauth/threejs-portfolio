be09229a6648fadf51dc440b04f1a276536634e6
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-12 08:48:57 -0800

    update CV layout

--- src/pages/CV.jsx
+++ src/pages/CV.jsx
@@ -18,45 +18,58 @@ export default function CV() {
         <li>
           Lighthouse Labs <br />
           <span className="text-purple-300">
-            ‣ Diploma, Web Development: 05/2024 - Victoria, BC (Remote online){" "}
+            ‣ Diploma, Web Development: 05/2024 <br />○ Victoria, BC (Remote
+            online){" "}
           </span>
         </li>
         <li>
           Concordia University <br />{" "}
           <span className="text-purple-300">
-            ‣ Bachelor of Fine Arts, Art Education: 06/2014 - Montreal, Quebec
+            ‣ Bachelor of Fine Arts, Art Education: 06/2014 <br />○ Montreal,
+            Quebec
           </span>
         </li>
       </ul>
       <hr className="my-2" />
       <ul>
         <li>
-          <b>This Website ;)</b>
+          <b>This Website</b>
           <br />
-          <span className="text-purple-300">‣ Built with React 3 Fiber</span>
+          <span className="text-purple-300">○ React 3 Fiber</span>
+        </li>
+        <li>
+          <b className="text-white hover:text-purple-200">
+            <a
+              href="https://migauth.github.io/braille-translator/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Braille Translator
+            </a>
+          </b>{" "}
+          <br />
+          <span className="text-purple-300">
+            ‣ A simple braille translator <br />○ Vue, JavaScript, TailwindCSS
+          </span>
         </li>
         <li>
           <b>Schmovies</b> <br />
           <span className="text-purple-300">
-            ‣ The movie suggestion app ○ ReactJS, Django, Python, PostgreSQL
-          </span>
-        </li>
-        <li>
-          <b>Braille Translator</b> <br />
-          <span className="text-purple-300">
-            ‣ A simple braille translator ○ Vue, JavaScript, TailwindCSS
+            ‣ The movie suggestion app <br />○ ReactJS, Django, Python,
+            PostgreSQL
           </span>
         </li>
         <li>
           <b>Jungle Rails</b> <br />
           <span className="text-purple-300">
-            ‣ A mini e-commerce app ○ Ruby on Rails, JavaScript, HTML, SCSS
+            ‣ A mini e-commerce app <br />○ Ruby on Rails, JavaScript, HTML,
+            SCSS
           </span>
         </li>
         <li>
           <b>Tweeter</b> <br />
           <span className="text-purple-300">
-            ‣ Twitter clone ○ JavaScript, HTML, CSS
+            ‣ Twitter clone <br />○ JavaScript, HTML, CSS
           </span>
         </li>
       </ul>
