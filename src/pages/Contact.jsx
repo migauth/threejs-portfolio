141b2d81488afec0479a2553cc0eaf6b84461aea
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-19 13:49:00 -0800

    add thank you 3d

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,42 @@
+/* eslint-disable react/no-unknown-property */
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { Canvas, useFrame } from "@react-three/fiber";
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader";
+import { Canvas, useFrame, useLoader } from "@react-three/fiber";
+import { useRef, useEffect } from "react";
+import * as THREE from "three";
+
+
+function Name( {color} ) {
+  const gltf = useLoader(GLTFLoader, "/models/thank-you.glb");
+  const ref = useRef();
+
+  // Circular rotation
+  useFrame((_, delta) => {
+    ref.current.rotation.z += 1 * delta;
+  });
+
+  useEffect(() => {
+    // Traverse the model and update the material color
+    gltf.scene.traverse((child) => {
+      if (child.isMesh) {
+        // Ensure the mesh has a material
+        child.material = new THREE.MeshStandardMaterial({ color });
+      }
+    });
+  }, [gltf, color]);
+
+  return (
+    <primitive
+      object={gltf.scene}
+      position={[0, 0, 0]}
+      scale={[0.3, 0.3, 0.3]}
+      rotation={[Math.PI / 2, 0, 0]}
+      ref={ref}
+    />
+  );
+}
 
 export default function Contact() {
   return (
@@ -32,7 +67,13 @@ export default function Contact() {
           For collaboration or general inquiries, please reach out by email or
           LinkedIn!
         </p>
-
+        <div className="flex flex-row w-full sm:w-4/5">
+          <Canvas camera={{ position: [0, 0, 3], fov: 15 }}>
+            <ambientLight intensity={3} color={"#ffffff"}/>
+            <pointLight intensity={2} />
+            <Name color="#A855F7"/>
+          </Canvas>
+        </div>
       </div>
       <small className="text-xl pb-4">© 2024 Michael Gauthier</small>
     </div>
