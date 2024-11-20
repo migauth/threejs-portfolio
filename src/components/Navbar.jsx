f7c2ffe1ddfef7dd61e4e09f67c59691746af1d3
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-20 11:56:06 -0800

    add transparent navbar at top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,29 @@
+import { useState, useEffect } from "react";
+import "./Navbar.css"
+
 export default function Navbar() {
   const scrollToTop = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
+  // For hiding navbar bg
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <nav className="fixed gap-4 flex flex-col text-4xl w-full z-10  bg-purple-500 p-4 items-start opacity-95 sm:flex-row">
+      <nav className={`navbar fixed gap-4 flex flex-col text-4xl w-full z-10  p-4 items-start opacity-95 sm:flex-row ${isScrolled ? "scrolled" : "at-top"}`}>
         <h1 id="logo" className="pr-4 text-purple-200 hover:text-white">
           <a href="#" onClick={scrollToTop}>Michael Gauthier</a>
         </h1>
