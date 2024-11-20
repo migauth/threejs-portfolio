b2a6a459a7bbb767db2e8edf6f3d3a0018b9ad01
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-20 11:19:26 -0800

    center loading screen

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,7 +3,7 @@ import "./Loading.css";
 export default function Loading() {
   return (
     <>
-      <div className="flex flex-col w-full h-full justify-center items-center">
+      <div className="flex flex-col w-screen h-screen justify-center items-center">
         <p className="text-2xl  text-yellow-300">Loading</p>
         <br />
         <div className="loader mt-2"></div>
