9465a5d100daa103bc88390ce67f5f0ef558844d
Michael Gauthier <mikegauthier222@hotmail.com>
2024-11-26 12:20:54 -0800

    move loading to Scene and remove dead code

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,9 @@ import About from "./pages/About";
 import CV from "./pages/CV";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
-import Loading from "./components/Loading";
 import Home from "./pages/Home";
-import { useEffect, useState } from "react";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setIsLoading(false);
-    }, 2000);
-  }, []); // Add an empty dependency array to run useEffect only once.
 
   return (
     <>
@@ -23,7 +14,7 @@ function App() {
             <Navbar />
           </header>
           <section id="home" className="opacity-95">
-            <Home setIsLoading={setIsLoading} />
+            <Home />
           </section>
           <div className="p-16 sm:p-20">
             <section id="about">
